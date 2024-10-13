f4afed50c318666a838f6401204a956bbe3eb0f6
Jose Ibargüen Posada <joseibarguenp@gmail.com>
2024-10-13 14:07:05 -0500

    Third point of control. Creation of logout, and login functions and routes.

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -2,9 +2,6 @@ import user from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
 import {generateAccesToken} from '../libs/jwt.js';
 
-
-
-
 export const register = async (req, res) => {
     const {email, username, password} = req.body;
     try {
@@ -22,4 +19,26 @@ export const register = async (req, res) => {
         res.status(500).json({message: error.message });
     }
 };
-export const login = (req, res) => res.send("login");
+export const login = async (req, res) => {
+    const {email, password} = req.body;
+    try {
+        const userFound = await user.findOne({ email });
+        if (!userFound) return res.status(400).json({message:"User not found"});
+        
+        const isMatch = await bcrypt.compare(password, userFound.password);
+        if (!isMatch) return res.status(400).json({message: "Password is not correct"});
+
+        const token = await generateAccesToken({id: userFound.id });
+        res.cookie("token", token);
+
+        res.send('Usuario logeado satisfactoriamente');
+    } catch (error) {
+        res.status(500).json({message: error.message });
+    }
+};
+export const logout = (req, res) => {
+    res.cookie('token',"",{
+        expires: new Date(0)
+    })
+    return res.sendStatus(200);
+}
